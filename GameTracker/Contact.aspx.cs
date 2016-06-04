5d414716aea78e562934b60a8fb2a2caf08eb531
Ascinocco <scinocco.a@gmail.com>
2016-06-04 15:47:37 -0400

    init commit

---
+++ GameTracker/Contact.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+/**
+ * Game Tracker
+ * By: Anthony Scinocco & Alex Andriisyn
+ * Contact Page Code Behind file
+ * http://asp-game-tracker.azurewebsites.net/Contact.aspx
+ */
+namespace GameTracker
+{
+    public partial class Contact : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /**
+         * This function sends the users form
+         * TODO: Complete form submission
+         * 
+         * return {void}
+         */ 
+        protected void SendButton_Click(object sender, EventArgs e)
+        {
+            // this is a placeholder for working code that sends email 
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
---
+++ GameTracker/User_Controls/Jumbotron.ascx
@@ -0,0 +1,17 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Jumbotron.ascx.cs" Inherits="GameTracker.User_Controls.Jumbotron" %>
+<!--
+        Game Tracker
+        By: Anthony Scinocco & Alex Andriisyn
+        Jumbotron User Control
+        -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-offset-2 col-md-8">
+            <div class="jumbotron" id="content" runat="server">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
